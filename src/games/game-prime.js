079985a2da6752bd2af4fa00c13ec2ccd77f1e71
Alexey Cherkasov <alcherkasov@gmail.com>
2018-06-02 18:49:35 +0500

    Version 1.0.2

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -5,7 +5,7 @@ import gameEngine from '../game-engine';
 const rule = 'Is this number prime?';
 
 const isPrime = (num) => {
-  if (num === 1 || num <= 0) return false;
+  if (num <= 1) return false;
 
   for (let i = 2; i < num / 2; i += 1) {
     if (num % i === 0) return false;
